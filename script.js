cc0d1815cbedbea0b2a05df9a06831763c49f82a
Somiya Ratna Behera <beherasomiyaratna@gmail.com>
2023-10-04 21:53:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function volume_sphere() {
     //Write your code here
-	let boxes = document.getElementsByTagName("input");
-	let r = boxes[0].value;
-	let volume = boxes[1];
-	volume.value = (4*3.14*(r**3))/3;
+	let radius = document.querySelector("#radius");
+	let volume = document.querySelector("#volume");
+	let r = radius.value;
+	let v = (4*3.14*(r**3))/3;
+	volume.value = v;
   
 } 
 
